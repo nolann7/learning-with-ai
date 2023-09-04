c5b8e3206d00e6d953dfa47f3ab6e8e7775c0644
Alex Ch <nolann709@gmail.com>
2023-09-04 20:22:30 +0300

    add toaster shadcn ui component

--- src/components/ui/toaster.tsx
+++ src/components/ui/toaster.tsx
@@ -1,5 +1,35 @@
-type ToasterPropType = {};
+"use client"
 
-export const Toaster = (props: ToasterPropType) => {
-  return <p>Toaster</p>;
-};
+import {
+  Toast,
+  ToastClose,
+  ToastDescription,
+  ToastProvider,
+  ToastTitle,
+  ToastViewport,
+} from "@/components/ui/toast"
+import { useToast } from "@/components/ui/use-toast"
+
+export function Toaster() {
+  const { toasts } = useToast()
+
+  return (
+    <ToastProvider>
+      {toasts.map(function ({ id, title, description, action, ...props }) {
+        return (
+          <Toast key={id} {...props}>
+            <div className="grid gap-1">
+              {title && <ToastTitle>{title}</ToastTitle>}
+              {description && (
+                <ToastDescription>{description}</ToastDescription>
+              )}
+            </div>
+            {action}
+            <ToastClose />
+          </Toast>
+        )
+      })}
+      <ToastViewport />
+    </ToastProvider>
+  )
+}
