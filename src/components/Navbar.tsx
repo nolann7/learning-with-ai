e5f2a4b22cb0ae1586a9108f55261874ce859aa8
Alex Ch <nolann709@gmail.com>
2023-09-12 19:06:58 +0300

    fix navbar css

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,7 +10,7 @@ type Props = {};
 const Navbar = async (props: Props) => {
   const session = await getAuthSession();
   return (
-    <nav className="fixed inset-x-0 top-0 bg-white dark:bg-gray-950 z-[10] h-fit border-b border-zinc-300 py-2">
+    <nav className="fixed inset-x-0 top-0 bg-white dark:bg-gray-950 z-[10] h-fit border-b border-zinc-300 py-2 px-1">
       <div className="flex items-center justify-center h-full gap-2 px-8 mx-auto sm:justify-between max-w-7xl">
         <Link href="/gallery" className="items-center hidden gap-2 sm:flex">
           <p className="rounded-lg border-2 border-b-4 border-r-4 border-black px-2 py-1 text-xl font-bold transition-all hover:-translate-y-[2px] md:block dark:border-white">
@@ -23,7 +23,10 @@ const Navbar = async (props: Props) => {
           </Link>
           {session?.user && (
             <>
-              <Link href="/create" className="mr-3">
+              <Link href="/create" className="hidden mr-3 max-sm:block">
+                Create
+              </Link>
+              <Link href="/create" className="hidden mr-3 sm:block">
                 Create Course
               </Link>
               <Link href="/settings" className="mr-3">
