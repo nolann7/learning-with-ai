ba44b5911fe7a3cb28a47043c6db880ac780c66f
Alex Ch <nolann709@gmail.com>
2023-09-13 13:36:44 +0300

    add auth route

---
+++ src/app/api/auth/credentials/route.ts
@@ -0,0 +1,15 @@
+/* import { NextResponse } from 'next/server';
+
+// /api/auth/credentials/
+export async function POST(req: Request, res: Response) {
+  const { username, password } = await req.json();
+  console.log({ username, password });
+
+  const user = {
+    id: '10000',
+    name: 'test_user',
+    credits: 10,
+  };
+  return NextResponse.json(user);
+}
+ */
