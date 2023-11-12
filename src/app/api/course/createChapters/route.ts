1742a418cb5355920d5faa3ffc66ca45f1e42763
Alex Ch <nolann709@gmail.com>
2023-11-12 14:03:16 +0200

    comment imageSearchTerm

--- src/app/api/course/createChapters/route.ts
+++ src/app/api/course/createChapters/route.ts
@@ -44,14 +44,14 @@ export async function POST(req: Request, res: Response) {
       },
     );
 
-    const imageSearchTerm = await strict_output(
-      'you are an AI capable of finding the most relevant image for a course',
-      `Please provide a good image search term for the title of a course about ${title}. This search term will be fed into the unsplash API, so make sure it is a good search term that will return good results`,
-      {
-        image_search_term: 'a good search term for the title of the course',
-      },
-    );
-    console.log({ output_units, imageSearchTerm });
+    // const imageSearchTerm = await strict_output(
+    //   'you are an AI capable of finding the most relevant image for a course',
+    //   `Please provide a good image search term for the title of a course about ${title}. This search term will be fed into the unsplash API, so make sure it is a good search term that will return good results`,
+    //   {
+    //     image_search_term: 'a good search term for the title of the course',
+    //   },
+    // );
+    console.log({ output_units /* imageSearchTerm */ });
     console.log('hoooraaay');
     // const course_image = await getUnsplashImage(
     //   imageSearchTerm.image_search_term,
