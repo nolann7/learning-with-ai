008763f2a43739d6404659d9fef5bdcda8595788
Alex Ch <nolann709@gmail.com>
2023-11-12 11:38:21 +0200

    pnpm lock file

--- src/lib/unsplash.ts
+++ src/lib/unsplash.ts
@@ -3,5 +3,10 @@ export const getUnsplashImage = async (query: string) => {
   const { data } = await axios.get(`
     https://api.unsplash.com/search/photos?per_page=1&query=${query}&client_id=${process.env.UNSPLASH_API_KEY}
     `);
+
+  // const response = await fetch(`
+  //   https://api.unsplash.com/search/photos?per_page=1&query=${query}&client_id=${process.env.UNSPLASH_API_KEY}
+  //   `);
+  // const data = await response.json();
   return data.results[0].urls.small_s3;
 };
